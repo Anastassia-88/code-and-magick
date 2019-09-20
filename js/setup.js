df42268ec2fed829c324ffba134bae9ccddf15fb
Anastassia-88 <anastassia.russak@gmail.com>
2019-09-20 07:47:42 +0200

    Работа с окном настройки

--- js/setup.js
+++ js/setup.js
@@ -1,4 +1,35 @@
 'use strict';
 
-var userDialog = document.querySelector(('.setup'));
+var userDialog = document.querySelector('.setup');
 userDialog.classList.remove('hidden');
+
+document.querySelector('.setup-similar').classList.remove('hidden');
+
+var similarListElement = document.querySelector('.setup-similar-list');
+var similarWizardTemplate = document.querySelector('#similar-wizard-template')
+  .content
+  .querySelector('.setup-similar-item');
+
+var WIZARD_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
+var WIZARD_SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
+var WIZARD_COAT_COLOR = ['rgb (101, 137, 164)', 'rgb (241, 43, 107)', 'rgb (146, 100, 161)', 'rgb (56, 159, 117)', 'rgb (215, 210, 55)', 'rgb (0, 0, 0)'];
+var WIZARD_EYES_COLOR = ['black', 'red', 'blue', 'yellow', 'green'];
+
+
+
+
+
+
+
+
+
+
+for (var i = 0; i < 4; i++) {
+  var wizardElement = similarWizardTemplate.cloneNode(true);
+  wizardElement.querySelector('.setup-similar-label').textContent = WIZARD_NAMES[i] + ' ' + WIZARD_SURNAMES[i];
+  wizardElement.querySelector('.wizard-coat').style.fill = WIZARD_COAT_COLOR[i];
+  wizardElement.querySelector('.wizard-eyes').style.fill = WIZARD_EYES_COLOR[i];
+
+  similarListElement.appendChild(wizardElement);
+}
+
