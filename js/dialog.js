560cee400dfe25e84a36d353791062dcc761c0de
Anastassia-88 <anastassia.russak@gmail.com>
2019-10-06 18:30:16 +0200

    Работа с сетью

--- js/dialog.js
+++ js/dialog.js
@@ -2,9 +2,16 @@
 
 (function () {
   var setup = document.querySelector('.setup');
+  var form = setup.querySelector('.setup-wizard-form');
   var setupOpen = document.querySelector('.setup-open');
   var setupClose = setup.querySelector('.setup-close');
   var setupHandler = setup.querySelector('.upload');
+  var userNameInput = setup.querySelector('.setup-user-name');
+
+  var METHOD_SAVE = 'POST';
+  var METHOD_LOAD = 'GET';
+  var URL_SAVE = 'https://js.dump.academy/code-and-magick';
+  var URL_LOAD = 'https://js.dump.academy/code-and-magick/data';
 
 
   // Перемещение окна настройки
@@ -65,6 +72,7 @@
   var openPopup = function () {
     setup.classList.remove('hidden');
     document.addEventListener('keydown', onPopupEscPress);
+    window.backend.ajax(window.setup.insertWizards, window.backend.onError, METHOD_LOAD, URL_LOAD);
   };
 
   var closePopup = function () {
@@ -88,5 +96,35 @@
 
   setupClose.addEventListener('keydown', function (evt) {
     window.util.isEnterEvent(evt, closePopup);
+  });
+
+
+  // Валидация поля ввода имени персонажа
+  userNameInput.addEventListener('invalid', function () {
+    if (userNameInput.validity.tooShort) {
+      userNameInput.setCustomValidity('Имя должно состоять минимум из 2-х символов');
+    } else if (userNameInput.validity.tooLong) {
+      userNameInput.setCustomValidity('Имя не должно превышать 25-ти символов');
+    } else if (userNameInput.validity.valueMissing) {
+      userNameInput.setCustomValidity('Обязательное поле');
+    } else {
+      userNameInput.setCustomValidity('');
+    }
+  });
+
+  userNameInput.addEventListener('input', function (evt) {
+    var target = evt.target;
+    if (target.value.length < 2) {
+      target.setCustomValidity('Имя должно состоять минимум из 2-х символов');
+    } else {
+      target.setCustomValidity('');
+    }
+  });
+
+  form.addEventListener('submit', function (evt) {
+    evt.preventDefault();
+    window.backend.ajax(function () {
+      setup.classList.add('hidden');
+    }, window.backend.onError, METHOD_SAVE, URL_SAVE, new FormData(form));
   });
 })();
--- js/setup.js
+++ js/setup.js
@@ -2,73 +2,34 @@
 
 (function () {
   var setup = document.querySelector('.setup');
-  var WIZARDS_AMOUNT = 4;
-  var userNameInput = setup.querySelector('.setup-user-name');
-  var similarListElement = document.querySelector('.setup-similar-list');
-  var similarWizardTemplate = document.querySelector('#similar-wizard-template')
-    .content
-    .querySelector('.setup-similar-item');
+  var SIMILAR_WIZARDS_AMOUNT = 4;
+  var similarWizardsList = setup.querySelector('.setup-similar-list');
+  var similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
 
-
-  // Валидация поля ввода имени персонажа
-  userNameInput.addEventListener('invalid', function () {
-    if (userNameInput.validity.tooShort) {
-      userNameInput.setCustomValidity('Имя должно состоять минимум из 2-х символов');
-    } else if (userNameInput.validity.tooLong) {
-      userNameInput.setCustomValidity('Имя не должно превышать 25-ти символов');
-    } else if (userNameInput.validity.valueMissing) {
-      userNameInput.setCustomValidity('Обязательное поле');
-    } else {
-      userNameInput.setCustomValidity('');
-    }
-  });
-
-  userNameInput.addEventListener('input', function (evt) {
-    var target = evt.target;
-    if (target.value.length < 2) {
-      target.setCustomValidity('Имя должно состоять минимум из 2-х символов');
-    } else {
-      target.setCustomValidity('');
-    }
-  });
-
-
-  // Блок похожих персонажей
-  // Функция генерации случайных данных
-  var createWizardsArray = function (wizardsAmount) {
-    var arr = [];
-
-    for (var i = 0; i < wizardsAmount; i++) {
-      arr.push({
-        name: window.util.getRandomItemFromArray(window.data.WIZARD_NAMES) + ' ' + window.util.getRandomItemFromArray(window.data.WIZARD_SURNAMES),
-        coatColor: window.util.getRandomItemFromArray(window.data.WIZARD_COAT_COLORS),
-        eyesColor: window.util.getRandomItemFromArray(window.data.WIZARD_EYES_COLORS)
-      });
-    }
-    return arr;
-  };
-
-  // Функция создания DOM-элемента на основе JS-объекта
   var renderWizard = function (wizard) {
     var wizardElement = similarWizardTemplate.cloneNode(true);
     wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
-    wizardElement.querySelector('.wizard-coat').style.fill = wizard.coatColor;
-    wizardElement.querySelector('.wizard-eyes').style.fill = wizard.eyesColor;
+    wizardElement.querySelector('.wizard-coat').style.fill = wizard.colorCoat;
+    wizardElement.querySelector('.wizard-eyes').style.fill = wizard.colorEyes;
     return wizardElement;
   };
 
-  // Функция заполнения блока DOM-элементами на основе массива JS-объектов
   var insertWizards = function (wizards) {
+
+    window.util.removeNodeContent(similarWizardsList);
     var fragment = document.createDocumentFragment();
-    for (var i = 0; i < wizards.length; i++) {
-      fragment.appendChild(renderWizard(wizards[i]));
+
+    for (var i = 0; i < SIMILAR_WIZARDS_AMOUNT; i++) {
+      var j = window.util.getRandomIntInclusive(0, wizards.length);
+      fragment.appendChild(renderWizard(wizards[j]));
     }
-    similarListElement.appendChild(fragment);
+    similarWizardsList.appendChild(fragment);
+
+    setup.querySelector('.setup-similar').classList.remove('hidden');
   };
 
-  var wizards = createWizardsArray(WIZARDS_AMOUNT);
-  insertWizards(wizards);
-
-  document.querySelector('.setup-similar').classList.remove('hidden');
+  window.setup = {
+    insertWizards: insertWizards,
+  };
 })();
 
